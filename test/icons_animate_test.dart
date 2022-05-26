a2811b9f3ddbfd3fcd814ebf61d5bb41bb42fd54
Luca Colazzo <luca.colazzo99@gmail.com>
2022-05-26 10:41:49 +0200

    added description

--- test/icons_animate_test.dart
+++ test/icons_animate_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('default always passing test', () {
     expect(true, true);
   });
 }
